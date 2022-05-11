570cdc78d7e503893aacba2552bf337e3ae23bd9
Gilver Rolando Ordoñez <admi.jmb@gmail.com>
2022-05-11 14:24:57 -0500

    lesson - 01

---
+++ src/exercices/basic-types.ts
@@ -0,0 +1,10 @@
+// lesson 1
+/**
+ * basic types and general concepts
+ */
+
+ const namePerson: string = "Rolando"
+ const hp: number | string = "FULL"
+ const isAlive: boolean = true
+ 
+ console.log(namePerson, hp, isAlive)

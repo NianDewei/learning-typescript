518317ce3bf5ceea230c60622d2e60ce4069cc0f
Gilver Rolando Ordoñez <admi.jmb@gmail.com>
2022-05-12 02:11:33 -0500

    lesson - 06 | Destructuring Arrays

---
+++ src/exercices/06-destructuring-arrays.ts
@@ -0,0 +1,16 @@
+// lesson 06
+/**
+ *  Destructuring arrays
+ */
+
+ const dbz: string[] = ["Goku","Vegeta","Trunks"]
+ const [ch1,ch2,ch3] = dbz
+ // const [,,ch3] = dbz
+ 
+ // console.log("Character 1: ", dbz[0])
+ // console.log("Character 2: ", dbz[1])
+ // console.log("Character 3: ", dbz[2])
+ // ------------------------------------
+ console.log("Character 1: ", ch1)
+ console.log("Character 2: ", ch2)
+ console.log("Character 3: ", ch3)
